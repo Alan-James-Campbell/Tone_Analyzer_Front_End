e6939be828fff6869ceed9eaf838757df434dbe8
Alan-James-Campbell <alan.campbell@imatrixsys.com>
2019-11-19 16:38:54 -0800

    s

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const bodyParser      = require('body-parser')
 const app = express()
 
 app
-  .use(express.static(path.join(__dirname, '/build'))) // Serve the static files from the React app
+  .use(express.static(path.join(__dirname, 'public'))) // Serve the static files from the React app
   .use(bodyParser.urlencoded({ extended: true }))               // Allows req.body
   .use(bodyParser.json())                                       // Parsing Middleware
   .use('/api', require('./expressAPI/index'))                   // Used for external APIs unrelated to AWS Serverless Architecture
-  .get('*', (req,res) => res.sendFile(path.join(__dirname+'/build/index.html')))  // Handles unknown requests
+  .get('*', (req,res) => res.sendFile(path.join(__dirname+'public/index.html')))  // Handles unknown requests
 
 const port = process.env.PORT || 5000
 app.listen(port)
